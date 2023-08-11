3643135226f4620d9290cf09647a253c93937616
hernan verar <hernanverar@gmail.com>
2023-08-11 15:45:19 -0700

    This commit is a WIP trying to figure out why my View Index is not reponding

---
+++ PierreSweets/Migrations/20230811220107_AddApplicationUser.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PierreSweets.Migrations
+{
+    public partial class AddApplicationUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Treats");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Price",
+                table: "Treats",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
